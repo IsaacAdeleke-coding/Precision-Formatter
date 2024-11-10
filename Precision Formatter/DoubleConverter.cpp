d3b58bf49f0172fb70f2b52df33545f09efd310c
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-11-10 13:53:29 -0800

    Add Precision Formatter project with reusable double-to-string converter
    - Created DoubleConverter.hpp: Header file declaring doubleToString function.
    - Created DoubleConverter.cpp: Implementation file defining doubleToString with configurable precision.
    - Created main.cpp: Contains main function for testing doubleToString functionality.
    - Organized project to demonstrate separation of declarations, definitions, and main logic.

---
+++ Precision Formatter/DoubleConverter.cpp
@@ -0,0 +1,8 @@
+//
+//  DoubleConverter.cpp
+//  Precision Formatter
+//
+//  Created by Isaac Adeleke on 11/10/24.
+//
+
+#include "DoubleConverter.hpp"
---
+++ Precision Formatter/DoubleConverter.hpp
@@ -0,0 +1,13 @@
+//
+//  DoubleConverter.hpp
+//  Precision Formatter
+//
+//  Created by Isaac Adeleke on 11/10/24.
+//
+
+#ifndef DoubleConverter_hpp
+#define DoubleConverter_hpp
+
+#include <stdio.h>
+
+#endif /* DoubleConverter_hpp */
