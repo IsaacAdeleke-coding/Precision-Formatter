2777f58d79a469346b1157a726fe0e1af478843e
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-11-10 13:35:05 -0800

    Initial Commit

---
+++ Precision Formatter/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Precision Formatter
+//
+//  Created by Isaac Adeleke on 11/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
